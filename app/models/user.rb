ca5a7a70d4181dfec6f13f1df62a62247a8da08f
Raghu Betina <rbetina@gmail.com>
2013-05-24 14:59:34 -0500

    Prevent case sensitive email duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :service_orders
 
   # Validations
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { :case_sensitive => false }
   validates :first_name, presence: true
   validates :last_name, presence: true
 
