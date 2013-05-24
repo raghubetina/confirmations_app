aad37a03e69f6d2d4113ec1d6772e19d7efaf85b
Raghu Betina <rbetina@gmail.com>
2013-05-23 22:03:10 -0500

    Completed secure sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :password_digest
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+
+  has_secure_password
 
   # Associations
   has_many :confirmations
